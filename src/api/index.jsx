e69f4e16e0c7a04b0e0bf849911758165195a9b6
Nikola <nikola.vuletic82@gmail.com>
2023-03-21 12:18:18 +0100

    destinations added

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -22,10 +22,10 @@ const GetHotelsData = async (latitude, longitude) => {
           // lang: 'en_US'
         },
         headers: {
-          // 'X-RapidAPI-Key': '870802e1afmshea42dfcce80c856p18270cjsndf4cfaf46923',    nikola.vuletic82
-          // 'X-RapidAPI-Key': '6ffb7f2b39mshdae315b36d0452dp1a8e3fjsnbf5e819af236',    nikola.vuletic.akvia
-                            // "7e62f59f63msh1c905264fef8196p178da3jsn98288327e497",    viavsnikola.vuletic 
-          // "X-RapidAPI-Key": '917b765184mshbe09b7d42d98d26p1eaaa0jsn81787e831ac4',    momo, ana english next
+          // 'X-RapidAPI-Key': '870802e1afmshea42dfcce80c856p18270cjsndf4cfaf46923',   nikola.vuletic82
+          // 'X-RapidAPI-Key': '6ffb7f2b39mshdae315b36d0452dp1a8e3fjsnbf5e819af236',   nikola.vuletic.akvia
+                            // "7e62f59f63msh1c905264fef8196p178da3jsn98288327e497",   viavsnikola.vuletic 
+          // "X-RapidAPI-Key": '917b765184mshbe09b7d42d98d26p1eaaa0jsn81787e831ac4',   momo, ana english next
           'X-RapidAPI-Key': '9d685bfa9fmshcc02f1e361a8f53p126d65jsnd35a55061c28',
           "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
         },
